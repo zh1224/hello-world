9de3a57f27a45cc685cae2eeee4249588980b382
zh1224 <1243271157@qq.com>
2024-07-24 15:04:24 +0800

    first commit

--- config.py
+++ config.py
@@ -5,18 +5,18 @@ from __future__ import print_function
 from __future__ import unicode_literals
 
 # Authentication for user filing issue (must have read/write access to repository to add issue to)
-USERNAME = ''
-TOKEN = ''
+USERNAME = 'zh1224'
+TOKEN = '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'
 
 # The repository to add this issue to
-REPO_OWNER = ''
-REPO_NAME = ''
+REPO_OWNER = 'zh1224'
+REPO_NAME = 'hello-world'
 
 # Set new submission url of subject
 NEW_SUB_URL = 'https://arxiv.org/list/cs/new'
 
 # Keywords to search
-KEYWORD_LIST = ["", ]
+KEYWORD_LIST = ["reinforcement", ]
 
 
 OPENAI_API_KEYS = ['', ]
