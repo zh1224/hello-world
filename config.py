9205d56e79de1f2f86fc9db8c91952c1ca3bd92d
zh1224 <64815278+zh1224@users.noreply.github.com>
2024-07-24 17:43:59 +0800

    Update config.py

--- config.py
+++ config.py
@@ -16,7 +16,7 @@ REPO_NAME = 'hello-world'
 NEW_SUB_URL = 'https://arxiv.org/list/cs/new'
 
 # Keywords to search
-KEYWORD_LIST = ["reinforcement learning","agent", ]
+KEYWORD_LIST = ["reinforcement learning agent", ]
 
 
 OPENAI_API_KEYS = ['sk-proj-LtiGrLr7HPw1bzPVPExST3BlbkFJBTX0Rm7WhTho0ZgbqjxA', ]
